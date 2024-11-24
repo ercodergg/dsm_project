b32d8be59b8d7e499c840fdecb78bd9fcafb51ec
ercodergg <ernest.garces.giner@gmail.com>
2024-11-24 12:13:11 +0100

    datos registro de cliente y restaurante recibidos en métodos diferentes según la elección del usuario

--- js/registrarse.js
+++ js/registrarse.js
@@ -1,26 +1,57 @@
 console.log("El script se ha cargado correctamente.");
-
 // Función para manejar el envío del formulario
 function handleSubmit(event) {
     // Evita el envío normal del formulario
     event.preventDefault();
-
-    const form = document.querySelector('form');
+    
+    const form = event.target; // Obtener el formulario
     const tipoUsuario = form.querySelector('#tipo-usuario').value; // Obtener el valor del input oculto
 
     if (tipoUsuario === 'Cliente') {
         console.log('Es un Cliente');
+        const nombre = form.querySelector('#nombre').value;
+        const pass = form.querySelector('#pass').value;
+        const pass2 = form.querySelector('#pass2').value;
+        const correoElectronico = form.querySelector('#correoElectronico').value;
+        const telefono = form.querySelector('#telefono').value;
+        const direccion = form.querySelector('#direccion').value;
+        registrarCliente(nombre, correoElectronico, pass, pass2, telefono, direccion);
     } else if (tipoUsuario === 'Restaurante') {
         console.log('Es un Restaurante');
+        const nombre = form.querySelector('#nombre').value;
+        const direccion = form.querySelector('#direccion').value;
+        const tipoCocina = form.querySelector('#tipoCocina').value;
+        const pass = form.querySelector('#pass').value;
+        const pass2 = form.querySelector('#pass2').value;
+        const correoElectronico = form.querySelector('#correoElectronico').value;
+        const telefono = form.querySelector('#telefono').value;
+        registrarRestaurante(nombre, direccion, tipoCocina, pass, pass2, correoElectronico, telefono);
     } else {
-        console.log('No se seleccionó tipo de usuario.');
+        console.log('Algo ha ocurrido...');
+        console.log('Tipo de Usuario:', tipoUsuario);
     }
 
-    // Aquí puedes agregar lógica de validación si es necesario
-    // Por ejemplo, validaciones adicionales antes de redirigir
-    // window.location.href = 'index.html';
+    return tipoUsuario;
 }
-
+function registrarCliente(nombre, correoElectronico, pass, pass2, telefono, direccion) {
+    console.log('Registrando cliente...');
+    console.log('Nombre:', nombre);
+    console.log('Correo Electrónico:', correoElectronico);
+    console.log('Contraseña:', pass);
+    console.log('Confirmar Contraseña:', pass2);
+    console.log('Teléfono:', telefono);
+    console.log('Dirección:', direccion);
+}
+function registrarRestaurante(nombre, direccion, tipoCocina, pass, pass2, correoElectronico, telefono) {
+    console.log('Registrando restaurante...');
+    console.log('Nombre:', nombre);
+    console.log('Dirección:', direccion);
+    console.log('Tipo de Cocina:', tipoCocina);
+    console.log('Contraseña:', pass);
+    console.log('Confirmar Contraseña:', pass2);
+    console.log('Correo Electrónico:', correoElectronico);
+    console.log('Teléfono:', telefono);
+}
 // Agregar el evento de escucha cuando el documento esté completamente cargado
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.querySelector('.buscar');
